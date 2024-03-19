bce01e7f3570d387f27cabe75cc89ccbf34639bf
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-03-19 13:44:18 +0800

    validating request with zod

--- next-app/app/api/users/[id]/route.tsx
+++ next-app/app/api/users/[id]/route.tsx
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-
+import schema from "../schema";
 
 export function GET(
     request: NextRequest, 
@@ -16,11 +16,22 @@ export async function PUT(
         /* Validate the request body */
         const body = await request.json();
 
-        if(!body.name)
-            return NextResponse.json({error: "Name is required"}, {status: 400})
+        const validation = schema.safeParse(body);
+
+        if(!validation.success)
+            return NextResponse.json(validation.error.errors, {status: 400})
 
         if(params.id > 10)
             return NextResponse.json({error: "User not found"}, {status: 404})
         
         return NextResponse.json({id: 1, name: body.name})
+}
+
+export function DELETE(
+    request: NextRequest, 
+    {params}:{ params: {id: number}}){
+    if(params.id > 10)
+        return NextResponse.json({error: "User not found"}, {status: 404})
+
+    return NextResponse.json({})
 }
