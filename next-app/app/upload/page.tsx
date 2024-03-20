44034ff82072b38ad4d5e4080700fda98cea1fda
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-03-20 08:22:38 +0800

    using cloudinary to upload and show image

--- next-app/app/upload/page.tsx
+++ next-app/app/upload/page.tsx
@@ -1,14 +1,38 @@
 "use client";
 
-import React from 'react';
-import { CldUploadWidget } from 'next-cloudinary';
+import React, { useState } from 'react';
+import { CldImage, CldUploadWidget } from 'next-cloudinary';
+
+interface CloudinaryResult{
+	public_id: string;
+}
 
 const UploadPage = () => {
-  return (
-    <CldUploadWidget uploadPreset='da2bpk2v'>
-        { ({open})=><button className='btn btn-primary' onClick={()=>open()}>Upload</button> }
-    </CldUploadWidget>
-  )
+
+	const [publicId, setPublicId] = useState("");
+
+	return (
+		<>
+			{publicId && <CldImage src={publicId} width={270} height={180} alt="nothing"/>}
+			<CldUploadWidget 
+				uploadPreset='da2bpk2v'
+				options={{
+					sources: ["local"],
+					multiple: false,
+					maxFiles: 5,
+				}}	
+				onUpload ={(result, widget) =>{
+					if(result.event !== "success") return;
+
+					const info = result.info as CloudinaryResult;
+					setPublicId(info.public_id);
+
+				}}
+			>
+					{ ({open})=><button className='btn btn-primary' onClick={()=>open()}>Upload</button> }
+			</CldUploadWidget>
+		</>
+	)
 }
 
 export default UploadPage;
